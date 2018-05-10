5bbda762ce76a489c9a0b0277ad534aff331f75a
Jordi <jordisvila@gmail.com>
2018-05-10 17:05:54 +0200

    Prova

--- app/src/main/java/com/example/jordi/project/MainActivity.java
+++ app/src/main/java/com/example/jordi/project/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+//Hola
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
