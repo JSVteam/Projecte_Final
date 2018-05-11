dce4fa12a44d0259e1412b873c2622d55d2d29d5
Jordi <jordisvila@gmail.com>
2018-05-11 17:46:44 +0200

    V2.2

--- app/src/main/java/com/example/jordi/project/PaginaPrincipal.java
+++ app/src/main/java/com/example/jordi/project/PaginaPrincipal.java
@@ -4,6 +4,8 @@ package com.example.jordi.project;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.SearchView;
+import android.view.MenuInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -61,6 +63,24 @@ public class PaginaPrincipal extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.menu_search, menu);
+        MenuItem item = menu.findItem(R.id.menuSeach);
+        SearchView searchView = (SearchView) item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
         getMenuInflater().inflate(R.menu.pagina_principal, menu);
         return true;
     }
