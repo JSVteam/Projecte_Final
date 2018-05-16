ecfbfc2b475f0732b050096c90d25eb4bbf4a137
Jordi <jordisvila@gmail.com>
2018-05-16 19:47:59 +0200

    V2.5

--- app/src/main/java/com/example/jordi/project/Entrar_Usuari.java
+++ app/src/main/java/com/example/jordi/project/Entrar_Usuari.java
@@ -1,14 +1,28 @@
 package com.example.jordi.project;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Entrar_Usuari extends AppCompatActivity {
 
     Button boto;
+    Button iniciar_sesion;
+    EditText email;
+    EditText contrasenya;
+    Boolean sesion_iniciada = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,15 +30,53 @@ public class Entrar_Usuari extends AppCompatActivity {
         boto = findViewById(R.id.boto_entrar);
 
 
+        final ProgressDialog progressDialog = new ProgressDialog(this,
+                R.style.Theme_AppCompat_DayNight_Dialog);
+        iniciar_sesion = findViewById(R.id.boto_entrar);
+        email = findViewById(R.id.tv_email);
+        contrasenya = findViewById(R.id.tv_Contrasenya);
 
-        boto.setOnClickListener(new View.OnClickListener() {
+        final Response.Listener<String> respoListern = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    boolean success = jsonResponse.getBoolean("success");
+                    if (success) {
+                        sesion_iniciada = true;
+                        //shared_Preferences.setSharedPreferences(getApplicationContext(), sesion_iniciada);
+                        Intent intent = new Intent(getApplicationContext(), PaginaPrincipal.class);
+                        startActivity(intent);
+                        progressDialog.dismiss();
+                        progressDialog.hide();
+                        finish();
+
+
+                    } else {
+                        progressDialog.dismiss();
+                        progressDialog.hide();
+                        Toast.makeText(getApplicationContext(), "No se ha podido iniciar sesión", Toast.LENGTH_SHORT).show();
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        iniciar_sesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Entrar_Usuari.this , PaginaPrincipal.class);
-                startActivity(intent);
+
+                // Crear un objecto de tipo inicar sesion con los datos introducido.
+                Login iniciar_sesion = new Login(email.getText().toString(), contrasenya.getText().toString(), respoListern);
+                RequestQueue requestQueue = Volley.newRequestQueue(Entrar_Usuari.this);
+                requestQueue.add(iniciar_sesion);
+                progressDialog.setIndeterminate(true);
+                progressDialog.setMessage("Iniciando sesión...");
+                progressDialog.show();
             }
         });
-
     }
 
 
---
+++ app/src/main/java/com/example/jordi/project/Login.java
@@ -0,0 +1,26 @@
+package com.example.jordi.project;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Login extends StringRequest{
+
+    private static final  String Iniciar_sesion_URI = "http://80.211.40.68/ProjecteFinal/iniciar_sesion.php";
+    private Map<String, String> params;
+    public Login(String email, String contrasenya, Response.Listener<String> listener){
+        super(Request.Method.POST, Iniciar_sesion_URI, listener, null);
+        params = new HashMap<>();
+        params.put("email", email);
+        params.put("contrasenya", contrasenya);
+    }
+    @Override
+    public Map<String, String> getParams() {
+
+        return params;
+    }
+
+}
