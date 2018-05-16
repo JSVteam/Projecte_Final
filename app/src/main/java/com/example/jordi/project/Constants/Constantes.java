b351ee70a5391604d1c60117660ad703c073a3d6
Jordi <jordisvila@gmail.com>
2018-05-16 16:40:00 +0200

    V2.5

--- app/src/main/java/com/example/jordi/project/Constants/Constantes.java
+++ app/src/main/java/com/example/jordi/project/Constants/Constantes.java
@@ -6,5 +6,5 @@ public class Constantes {
     public static final String API_KEY = "api_key";
     public static final String LANGUAGE = "language";
     public static final String PAGE = "page";
-    public static final String BASE_URL_IMAGE = "http://image.tmdb.org/t/p/w200";
+    public static final String BASE_URL_IMAGE = "http://image.tmdb.org/t/p/w300";
 }
--- app/src/main/java/com/example/jordi/project/MainActivity.java
+++ app/src/main/java/com/example/jordi/project/MainActivity.java
@@ -40,8 +40,8 @@ public class MainActivity extends AppCompatActivity {
         button2 = findViewById(R.id.button3);
 
         imatge = (ImageView) findViewById(R.id.imageLogo) ;
-        String url = "https://cdn.vox-cdn.com/thumbor/AwKSiDyDnwy_qoVdLPyoRPUPo00=/39x0:3111x2048/1400x1400/filters:focal(39x0:3111x2048):format(png)/cdn.vox-cdn.com/uploads/chorus_image/image/49901753/netflixlogo.0.0.png";
-        Picasso.with(this).load(url).into(imatge);
+        //String url = "https://logo.clearbit.com/www.jobs.netflix.com";
+       // Picasso.with(this).load(url).into(imatge);
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(Constantes.URL)
@@ -61,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
 
                 button.setText(firstSerie.getFirst_air_date());
                 button2.setText(firstSerie.getName());
+                carregarImageURL(Constantes.BASE_URL_IMAGE+firstSerie.getPoster_path(), imatge);
             }
 
             @Override
